1e74fb131c8386732c356e8b567c3a4a61282326
Abdelhak HAFFAF <abdelhak.haffaf@gmail.com>
2016-05-09 16:28:29 +0200

    trigger only on Dockerfile call

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Configure git
-if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_EMAIL" ]; then
-    echo >&2 "[Git] You don't have specified the required env variables. "
-    echo >&2 "Check if you have configure GIT_USERNAME and GIT_EMAIL"
-    exit 1;
-fi
+# Trigger only on Dockerfile
+if [ "$1" = "php5-fpm" ]; then
+    # Configure git
+    if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_EMAIL" ]; then
+        echo >&2 "[Git] You don't have specified the required env variables. "
+        echo >&2 "Check if you have configure GIT_USERNAME and GIT_EMAIL"
+        exit 1;
+    fi
 
-git config --global user.name "$GIT_USERNAME"
-git config --global user.email "$GIT_EMAIL"
+    git config --global user.name "$GIT_USERNAME"
+    git config --global user.email "$GIT_EMAIL"
+fi
 
 exec "$@"
